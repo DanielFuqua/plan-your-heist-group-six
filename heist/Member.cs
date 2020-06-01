424cf92eb18bc7a19c96b9c44e7ffe61962337b2
robdan89 <fuquardwork@gmail.com>
2020-06-01 11:23:01 -0500

    able to print new user name skill and courage

---
+++ heist/Member.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace heist
+{
+    public class Member
+    {
+        public string Name { get; set; }
+
+        public int Skill { get; set; }
+
+        public double Courage { get; set; }
+
+
+    }
+
+
+}
--- heist/Program.cs
+++ heist/Program.cs
@@ -6,7 +6,35 @@ namespace heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine();
+            Console.WriteLine("Create a Team Member.");
+            // Prompt the user to enter a team member's name and save that name.
+            Console.WriteLine("Enter team member's name");
+
+            Console.Write("Name> ");
+            string userName = Console.ReadLine();
+
+            // Prompt the user to enter a team member's skill level and save that skill level with the name.
+            Console.WriteLine($"Welcome to the team, {userName}! What's your skill level?");
+
+            Console.Write("Skill> ");
+            int userSkill = int.Parse(Console.ReadLine());
+
+            // Prompt the user to enter a team member's courage factor and save that courage factor with the name.
+            Console.Write("Courage> ");
+            double userCourage = double.Parse(Console.ReadLine());
+
+            // Instantiate a member object
+            Member newMember = new Member()
+            {
+                Name = userName,
+                Skill = userSkill,
+                Courage = userCourage
+            };
+
+            Console.WriteLine($"Name: {newMember.Name} Skill: {newMember.Skill} Courage :{newMember.Courage}");
+
         }
     }
 }
